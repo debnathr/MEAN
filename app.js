b5e96e62aa9143ae40abdb26d785825b475d1048
rubi_1992 <er.rubidebnath.2014@gmail.com>
2016-01-23 14:03:42 +0530

    mean stack application

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module('flapperNews', []);
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+app.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts){
+		$scope.test = 'Hello world!';
+		$scope.posts = posts.posts;
+		// $scope.posts = [
+		// {title: 'post 1', upvotes: 5},
+		// {title: 'post 2', upvotes: 6},
+		// {title: 'post 3', upvotes: 3},
+		// {title: 'post 4', upvotes: 7},
+		// {title: 'post 5', upvotes: 2},
+		// {title: 'post 6', upvotes: 9}
+		// ];
+	$scope.addPost = function(){
+		if (!$scope.title || $scope.title === '') {return;};
+	$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+	$scope.title = '';
+	$scope.link = '';
+};
+
+$scope.incrementUpvotes = function(post){
+	post.upvotes += 1;
+};
+
+	}]);
+
